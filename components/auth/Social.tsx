bb3f704cd3ee5621a99fd1fce61793347e6964ec
NekoJar <mzharfanhakim7@gmail.com>
2024-01-23 11:42:57 +0700

    adding a login form

--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -8,6 +8,9 @@ export const Social = () => {
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+        <FcGoogle className="h-5 w-5" />
+      </Button>
+      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
