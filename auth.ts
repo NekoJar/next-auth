6e84e6f4b4aa385a048356940160d3e400c88d56
NekoJar <mzharfanhakim7@gmail.com>
2024-01-26 23:21:41 +0700

    updating types for callback function

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
 
 export const {
   handlers: { GET, POST },
@@ -10,14 +12,24 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
-    async session({ session, token }) {
+    async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
 
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
       return session;
     },
-    async jwt({ token, user, account }) {
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+      token.role = existingUser.role;
       return token;
     },
   },
